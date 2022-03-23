9d8d7dd59a57ebebcf4ff875c25de7688acf0637
Narendranath Panda <3488520+naren4b@users.noreply.github.com>
2022-03-23 22:49:22 +0530

    Create bootstrap.sh

---
+++ vagrant/bootstrap.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+apt update -y 
+apt install net-tools -y 
+
+echo "[TASK 1] Enable ssh password authentication"
+sed -i 's/^PasswordAuthentication .*/PasswordAuthentication yes/' /etc/ssh/sshd_config
+echo 'PermitRootLogin yes' >> /etc/ssh/sshd_config
+systemctl reload sshd
+
+echo "[TASK 2] Set root password"
+echo -e "root123\nroot123" | passwd root >/dev/null 2>&1
+echo "export TERM=xterm" >> /etc/bash.bashrc
+
+echo "[TASK 10] Update /etc/hosts file"
+cat >>/etc/hosts<<EOF
+172.16.16.100   vm.naren.local     
+EOF
