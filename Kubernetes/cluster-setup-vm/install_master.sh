99bd5a82ff5c1f118e461cc0fe20aef66aeee03b
Narendranath Panda <3488520+naren4b@users.noreply.github.com>
2022-05-01 22:11:42 +0530

    Update install_master.sh

--- Kubernetes/cluster-setup-vm/install_master.sh
+++ Kubernetes/cluster-setup-vm/install_master.sh
@@ -138,7 +138,19 @@ sudo cp -i /etc/kubernetes/admin.conf ~/.kube/config
 
 kubectl apply -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')"
 
+### To untaint 
+echo kubectl taint node $(hostname) node-role.kubernetes.io/master-
+
+### install storage class 
+echo kubectl create -f https://raw.githubusercontent.com/rancher/local-path-provisioner/master/deploy/local-path-storage.yaml
+
+### install ingress controller 
+echo kubectl label nodes  $(hostname) --overwrite=true app.kubernetes.io/component=controller  app.kubernetes.io/instance=ingress-nginx  app.kubernetes.io/name=ingress-nginx ingress-ready=true kubernetes.io/os=linux 
+echo kubectl create -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/main/deploy/static/provider/kind/deploy.yaml
+
 echo
 echo "### COMMAND TO ADD A WORKER NODE ###"
 kubeadm token create --print-join-command --ttl 0
+
+
 
